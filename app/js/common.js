d8859dd0763c38a15bc443bf22beb3c9f74cbf0c
Julia Vangeliy <vangeliy_yulia@mail.ru>
2018-09-05 23:01:39 +0300

    Slider Pt.1

---
+++ app/js/common.js
@@ -0,0 +1,13 @@
+$(".slider__list").owlCarousel({
+	items : 1,
+	nav : true,
+	navText : "",
+	loop : true,
+	autoplay : false,
+	autoplayHoverPause : true,
+	fluidSpeed : 600,
+	autoplaySpeed : 600,
+	navSpeed : 600,
+	dotsSpeed : 600,
+	dragEndSpeed : 600
+});
